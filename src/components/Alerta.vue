aa9c351514e5fa1ed4c8d09638b93a6baa722241
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-09 18:43:38 +0200

    creando un componente para mostrar las alertas

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    alerta: {
+        type: Object,
+        required: true
+    }
+})
+
+const isError = computed(() => {
+    return 'Esta es la funcion'
+})
+
 
 </script>
 
 <template>
-    <div class=""></div>
+    <div :class="isError" class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md bg-red-500">
+        {{ alerta.mensaje }}
+    </div>
 </template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -35,6 +35,7 @@ const validar = () => {
             Añade Pacientes y
             <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
+        <Alerta v-if="alerta.mensaje" :alerta="alerta" />
 
         <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" @submit.prevent="validar">
 
