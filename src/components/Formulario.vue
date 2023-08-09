c90073e3dbe9d1902e89b57a1bc74415cbd394e6
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-09 16:42:04 +0200

    primeros pasos con el formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -10,6 +10,17 @@
             Añade Pacientes y
             <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
+
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <div class="mb-5">
+                <label for="mascota" class="block text-gray-700 uppercase font-bold">
+                    Nombre Mascota
+                </label>
+
+                <input id="mascota" type="text" placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+            </div>
+        </form>
     </div>
 </template>
 
