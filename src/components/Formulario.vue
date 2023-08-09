4470e1022060cc2932863951155dcf20c710152a
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-09 17:02:30 +0200

    finalizando el formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -20,6 +20,42 @@
                 <input id="mascota" type="text" placeholder="Nombre de la mascota"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
             </div>
+
+            <div class="mb-5">
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">
+                    Nombre Propietario
+                </label>
+
+                <input id="propietario" type="text" placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+            </div>
+
+
+            <div class="mb-5">
+                <label for="email" class="block text-gray-700 uppercase font-bold">
+                    Email Propietario
+                </label>
+
+                <input id="email" type="text" placeholder="Email del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+            </div>
+
+            <div class="mb-5">
+                <label for="alta" class="block text-gray-700 uppercase font-bold">
+                    Alta
+                </label>
+
+                <input id="alta" type="date" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+            </div>
+
+            <div class="mb-5">
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">
+                    Sintomas
+                </label>
+
+                <textarea id="sintomas" type="text" class="border-2 w-full p-2 mt-2  rounded-md h-40" />
+            </div>
+
         </form>
     </div>
 </template>
